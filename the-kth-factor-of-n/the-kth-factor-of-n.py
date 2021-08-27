c2b68ecaa99abf361f062205a0aa60fb61741c13
Sadia Afrin Purba <36800937+SadiaAfrinPurba@users.noreply.github.com>
2021-08-27 12:54:28 +0600

    Time: 69 ms (5.29%), Space: 14.1 MB (91.99%) - LeetHub

---
+++ the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        
+        factors = [i for i in range(1, n+1) if n % i == 0]
+        
+        try:
+            return factors[k-1]
+        except IndexError:
+            return -1   
