925f67e5dc33e50caba333e5331490c95de85422
Sadia Afrin Purba <36800937+SadiaAfrinPurba@users.noreply.github.com>
2021-09-07 20:47:35 +0600

    Time: 56 ms (89.57%), Space: 15.2 MB (54.77%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,8 +1,13 @@
 class Solution:
-        def twoSum(self, nums: List[int], target: int) -> List[int]:
-            complement_mapper = {}
-            for i in range(len(nums)):
-                complement = target - nums[i]
-                if complement in complement_mapper:
-                    return [i, complement_mapper[complement]]
-                complement_mapper[nums[i]] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complement_mapper = {}
+        
+        for i in range(len(nums)):
+            compliment = target - nums[i]
+            if compliment in complement_mapper:
+                return [i, complement_mapper[compliment]]
+            complement_mapper[nums[i]] = i 
+            
+                
+        
+        
