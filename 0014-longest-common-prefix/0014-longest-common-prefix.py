24817bad5417c30cf82c1ce5ea054fa55bc82b20
Sadia Afrin Purba <purba0101@gmail.com>
2025-12-11 15:34:42 -0500

    Time: 0 ms (100%), Space: 17.9 MB (47.64%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+
+        for idx in range(len(strs[0])):
+            for s in strs:
+                if idx == len(s) or s[idx] != strs[0][idx]:
+                    return res
+            res += strs[0][idx]
+        
+        return res
